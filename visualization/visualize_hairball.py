74097779cfe4f49945a5494e262ca37844d4022a
skblaz <skrljblaz@gmail.com>
2018-04-13 13:32:52 +0200

    added a visualization example

--- visualization/visualize_hairball.py
+++ visualization/visualize_hairball.py
@@ -3,6 +3,5 @@ from py3plex.visualization.colors import all_color_names
 from py3plex.core import multinet
 
 multilayer_network = multinet.multi_layer_network().load_network("../experimental_networks/intact05.gpickle",directed=False,label_delimiter="---",input_type="gpickle")
-#network_colors, graph = multilayer_network.get_layers(style="hairball")
-#hairball_plot(graph,network_colors)
-#plt.show()
+hairball_plot(multilayer_network.core_network,"b",layered=False)
+plt.show()
