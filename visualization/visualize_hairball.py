b2bae542e74209ebf81f8d1ac4985501d4c85a08
SkBlaz <skrljblaz@gmail.com>
2018-04-15 07:30:19 +0200

    minor image change

--- visualization/visualize_hairball.py
+++ visualization/visualize_hairball.py
@@ -9,5 +9,5 @@ parser_init.add_argument("--input_graph", help="Load graph file")
 parser = parser_init.parse_args()
 
 multilayer_network = multinet.multi_layer_network().load_network(parser.input_graph,directed=False,label_delimiter="---",input_type="gpickle")
-hairball_plot(multilayer_network.core_network,"b",layered=False,legend=False)
+hairball_plot(multilayer_network.core_network,"b",layered=False,legend=False,layout_parameters={'iterations':100})
 plt.show()
